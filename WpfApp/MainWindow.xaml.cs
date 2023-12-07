66ecc9c94eead9ec0a69f7f257485881d78fae02
BAAL98 <baal98@gmail.com>
2023-12-07 14:04:00 +0200

    Added text function

--- WpfApp/MainWindow.xaml.cs
+++ WpfApp/MainWindow.xaml.cs
@@ -2,6 +2,8 @@
 using System.IO;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Ink;
+using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 
@@ -9,6 +11,8 @@ namespace WpfApp
 {
     public partial class MainWindow : Window
     {
+        private TextBlock SelectedTextBlock; // Държи референция към избрания текстов обект
+
         public MainWindow()
         {
             InitializeComponent();
@@ -19,26 +23,26 @@ namespace WpfApp
         {
             var button = sender as Button;
             var color = (Color)ColorConverter.ConvertFromString(button.Tag.ToString());
-            inkCanvas.DefaultDrawingAttributes.Color = color;
+            rotatableInkCanvas.DefaultDrawingAttributes.Color = color;
         }
 
         private void SliderThickness_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
         {
-            if (inkCanvas != null)
+            if (rotatableInkCanvas != null)
             {
-                inkCanvas.DefaultDrawingAttributes.Width = sliderThickness.Value;
-                inkCanvas.DefaultDrawingAttributes.Height = sliderThickness.Value;
+                rotatableInkCanvas.DefaultDrawingAttributes.Width = sliderThickness.Value;
+                rotatableInkCanvas.DefaultDrawingAttributes.Height = sliderThickness.Value;
             }
         }
 
         private void SelectMode(object sender, RoutedEventArgs e)
         {
-            inkCanvas.EditingMode = InkCanvasEditingMode.Select;
+            rotatableInkCanvas.EditingMode = InkCanvasEditingMode.Select;
         }
 
         private void EraseMode(object sender, RoutedEventArgs e)
         {
-            inkCanvas.EditingMode = InkCanvasEditingMode.EraseByPoint;
+            rotatableInkCanvas.EditingMode = InkCanvasEditingMode.EraseByPoint;
         }
 
         private void SaveInk(object sender, RoutedEventArgs e)
@@ -52,8 +56,8 @@ namespace WpfApp
 
             if (saveFileDialog.ShowDialog() == true)
             {
-                var renderTargetBitmap = new RenderTargetBitmap((int)inkCanvas.ActualWidth, (int)inkCanvas.ActualHeight, 96d, 96d, PixelFormats.Default);
-                renderTargetBitmap.Render(inkCanvas);
+                var renderTargetBitmap = new RenderTargetBitmap((int)rotatableInkCanvas.ActualWidth, (int)rotatableInkCanvas.ActualHeight, 96d, 96d, PixelFormats.Default);
+                renderTargetBitmap.Render(rotatableInkCanvas);
                 var jpegBitmapEncoder = new JpegBitmapEncoder();
                 jpegBitmapEncoder.Frames.Add(BitmapFrame.Create(renderTargetBitmap));
 
@@ -66,7 +70,92 @@ namespace WpfApp
 
         private void DrawingMode(object sender, RoutedEventArgs e)
         {
-            inkCanvas.EditingMode = InkCanvasEditingMode.Ink;
+            rotatableInkCanvas.EditingMode = InkCanvasEditingMode.Ink;
         }
+
+        private void RoundBrush(object sender, RoutedEventArgs e)
+        {
+            rotatableInkCanvas.DefaultDrawingAttributes.StylusTip = StylusTip.Ellipse;
+        }
+
+        private void RectangleBrush(object sender, RoutedEventArgs e)
+        {
+            rotatableInkCanvas.DefaultDrawingAttributes.StylusTip = StylusTip.Rectangle;
+        }
+
+        private void RotateButton_Click(object sender, RoutedEventArgs e)
+        {
+            rotatableInkCanvas.RotateSelectedStrokes(15); // Въртене на 15 градуса
+        }
+
+        private void SelectButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Този код изисква допълнителна логика за избиране на конкретен Stroke
+            rotatableInkCanvas.SelectStroke(rotatableInkCanvas.Strokes[0]); 
+        }
+
+        private void ChangeSelectedStrokesColor(object sender, RoutedEventArgs e)
+        {
+            if (rotatableInkCanvas.GetSelectedStrokes().Count == 0) return;
+
+            ComboBoxItem selectedColorItem = colorPicker.SelectedItem as ComboBoxItem;
+            if (selectedColorItem == null) return;
+
+            string colorName = selectedColorItem.Tag.ToString();
+            Color newColor = (Color)ColorConverter.ConvertFromString(colorName);
+
+            foreach (Stroke stroke in rotatableInkCanvas.GetSelectedStrokes())
+            {
+                stroke.DrawingAttributes.Color = newColor;
+            }
+        }
+
+        private void TextBlock_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (sender is TextBlock textBlock)
+            {
+                SelectedTextBlock = textBlock; // Съхраняване на избрания TextBlock
+                textToAdd.Text = textBlock.Text; // Зареждане на текста в полето за редактиране
+            }
+        }
+
+        private void AddTextButton_Click(object sender, RoutedEventArgs e)
+        {
+            TextBlock newTextBlock = new TextBlock
+            {
+                Text = textToAdd.Text,
+                Foreground = new SolidColorBrush(Colors.Black),
+                Background = new SolidColorBrush(Colors.Transparent)
+            };
+
+            // Задаване на MouseDown обработчика извън инициализатора на обекта
+            newTextBlock.MouseDown += TextBlock_MouseDown;
+
+            rotatableInkCanvas.Children.Add(newTextBlock);
+            Canvas.SetLeft(newTextBlock, 100); // Примерна позиция
+            Canvas.SetTop(newTextBlock, 100);
+        }
+
+        private void EditSelectedTextButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (SelectedTextBlock != null)
+            {
+                // Промяна на текста
+                SelectedTextBlock.Text = textToAdd.Text;
+
+                // Промяна на цвета на текста
+                ComboBoxItem selectedTextColorItem = textColorPicker.SelectedItem as ComboBoxItem;
+                if (selectedTextColorItem != null)
+                {
+                    string colorName = selectedTextColorItem.Tag.ToString();
+                    Color newColor = (Color)ColorConverter.ConvertFromString(colorName);
+                    SelectedTextBlock.Foreground = new SolidColorBrush(newColor);
+                }
+
+                // Промяна на размера на текста
+                SelectedTextBlock.FontSize = textSizeSlider.Value;
+            }
+        }
+
     }
 }
---
+++ WpfApp/RotatableInkCanvas.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Windows.Controls;
+using System.Windows.Ink;
+using System.Windows.Media;
+using System.Windows;
+using System.Windows.Input;
+
+namespace WpfApp
+{
+    public class RotatableInkCanvas : InkCanvas
+    {
+        protected override void OnStrokeCollected(InkCanvasStrokeCollectedEventArgs e)
+        {
+            base.OnStrokeCollected(e);
+            // Тук може да добавим логика, ако е нужно да променим новосъздадения Stroke
+        }
+
+        public void RotateSelectedStrokes(double angle)
+        {
+            var selectedStrokes = this.GetSelectedStrokes();
+
+            if (selectedStrokes.Count == 0) return;
+
+            // Намиране на центъра на селектирания обект/обекти
+            Rect bounds = selectedStrokes.GetBounds();
+            Point center = new Point(bounds.X + bounds.Width / 2, bounds.Y + bounds.Height / 2);
+
+            foreach (var stroke in selectedStrokes)
+            {
+                // Въртене на всяка точка в чертата
+                for (int i = 0; i < stroke.StylusPoints.Count; i++)
+                {
+                    var point = stroke.StylusPoints[i];
+                    var rotatedPoint = RotatePoint(point, center, angle);
+                    stroke.StylusPoints[i] = new StylusPoint(rotatedPoint.X, rotatedPoint.Y);
+                }
+            }
+        }
+
+        private Point RotatePoint(StylusPoint point, Point center, double angle)
+        {
+            double radAngle = (Math.PI / 180) * angle;
+            double cosTheta = Math.Cos(radAngle);
+            double sinTheta = Math.Sin(radAngle);
+
+            var x = cosTheta * (point.X - center.X) - sinTheta * (point.Y - center.Y) + center.X;
+            var y = sinTheta * (point.X - center.X) + cosTheta * (point.Y - center.Y) + center.Y;
+
+            return new Point(x, y);
+        }
+
+        public void SelectStroke(Stroke stroke)
+        {
+            // Пример за селектиране на Stroke
+            this.Select(new StrokeCollection { stroke });
+        }
+
+        private void RotateStroke(Stroke stroke, double angle)
+        {
+            // Примерна логика за ротация на черта
+            Matrix rotationMatrix = new Matrix();
+            Rect strokeBounds = stroke.GetBounds();
+            Point center = new Point(strokeBounds.X + strokeBounds.Width / 2, strokeBounds.Y + strokeBounds.Height / 2);
+
+            rotationMatrix.RotateAt(angle, center.X, center.Y);
+
+            // Променяме на трансформацията към точките на чертата
+            stroke.Transform(rotationMatrix, false);
+        }
+    }
+}
